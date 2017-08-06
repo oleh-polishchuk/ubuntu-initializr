cdf5943eece9f0478c7840aa1f233f62cda079e2
Polishchuk Oleh <Polishchuk Oleh>
2017-08-06 19:32:16 +0300

    1) Update installers. 2) Use pm2

--- app/db/installers.js
+++ app/db/installers.js
@@ -24,6 +24,69 @@ let installers = [
             'apt-get install -y build-essential'
         ]
     },
+    {
+        id: 4,
+        name: 'Grunt',
+        cmds: [
+            'apt-get install -y g++',
+            'apt-get install -y libkrb5-dev',
+            'npm install -g grunt-cli',
+        ]
+    },
+    {
+        id: 5,
+        name: 'Bower',
+        cmds: [
+            'npm install -g bower',
+        ]
+    },
+    {
+        id: 6,
+        name: 'Gulp',
+        cmds: [
+            'npm install -g gulpjs/gulp-cli',
+            'npm install gulpjs/gulp#4.0 -D',
+        ]
+    },
+    {
+        id: 7,
+        name: 'Yarn',
+        cmds: [
+            'curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -',
+            'echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list',
+            'apt-get update -y',
+            'apt-get install -y yarn',
+        ]
+    },
+    {
+        id: 8,
+        name: 'Java',
+        cmds: [
+            'apt-add-repository -y ppa:webupd8team/java',
+            'apt-get update -y',
+            'apt-get install -y oracle-java8-installer',
+            'java -version',
+        ]
+    },
+    {
+        id: 9,
+        name: 'Gradle',
+        cmds: [
+            'add-apt-repository -y ppa:cwchien/gradle',
+            'apt-get update -y',
+            'apt-get install -y gradle',
+        ]
+    },
+    {
+        id: 10,
+        name: 'Maven',
+        cmds: [
+            'apt-get purge -y maven maven2 maven3',
+            'add-apt-repository -y ppa:andrei-pozolotin/maven3',
+            'apt-get update -y',
+            'apt-get install -y maven3',
+        ]
+    },
 ];
 
 module.exports.getAll = function () {
