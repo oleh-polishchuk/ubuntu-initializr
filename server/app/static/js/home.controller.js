f8789ae55fc9ec985064d69faad72c428d44bb75
Oleh Polishchuk <oleh.polishchuk.ck@gmail.com>
2018-02-06 09:25:35 +0200

    Add uib-tooltip that show script under installer.

--- server/app/static/js/home.controller.js
+++ server/app/static/js/home.controller.js
@@ -3,7 +3,7 @@
     'use strict';
 
     angular
-        .module('app', [])
+        .module('app', ['ui.bootstrap'])
         .controller('HomeController', HomeControllerClass);
 
     HomeControllerClass.$inject = ['$http', '$window'];
